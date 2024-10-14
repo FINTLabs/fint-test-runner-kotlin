376bd6ce99fd6ed2936c75723e5d884db2fe92bd
Hknots <hknots99@gmail.com>
2024-10-14 09:12:40 +0200

    add more logging & get different endpoint index

--- src/main/kotlin/no/fintlabs/testrunner/resource/ResourceRepository.kt
+++ src/main/kotlin/no/fintlabs/testrunner/resource/ResourceRepository.kt
@@ -8,9 +8,11 @@ class ResourceRepository {
     val resources: MutableMap<String, MutableList<String>> = mutableMapOf()
 
     fun getResources(endpoint: String): MutableList<String>? {
+        var createComponentKey = createComponentKey(endpoint)
+        println("componentKey: $createComponentKey resources: $resources")
         return resources[createComponentKey(endpoint)]
     }
 
-    fun createComponentKey(endpoint: String) = endpoint.split("/").let { "${it[0]}.${it[1]}" }
+    fun createComponentKey(endpoint: String) = endpoint.split("/").let { "${it[1]}.${it[2]}" }
 
 }
