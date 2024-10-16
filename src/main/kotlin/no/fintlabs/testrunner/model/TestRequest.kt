50d2a4cd035e13f55ace423e1a6c125428c7ad93
Hknots <hknots99@gmail.com>
2024-10-16 08:42:07 +0200

    add regex validation on fields

--- src/main/kotlin/no/fintlabs/testrunner/model/TestRequest.kt
+++ src/main/kotlin/no/fintlabs/testrunner/model/TestRequest.kt
@@ -1,7 +1,28 @@
 package no.fintlabs.testrunner.model
 
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.Pattern
+
+
 data class TestRequest(
+    @field:NotBlank(message = "baseUrl must not be empty")
+    @field:Pattern(
+        regexp = "^https://[A-Za-z0-9\\-]+\\.felleskomponent\\.no",
+        message = "baseUrl must start with 'https://(string or -).felleskomponent.no'"
+    )
     val baseUrl: String,
+
+    @field:NotBlank(message = "endpoint must not be empty")
+    @field:Pattern(
+        regexp = "^/([a-zA-Z]+)(/[a-zA-Z]+)*\$",
+        message = "endpoint must start with '/' and contain valid path segments without a trailing slash"
+    )
     val endpoint: String,
+
+    @field:NotBlank(message = "clientName must not be empty")
+    @field:Pattern(
+        regexp = "^.+@client\\.[^.]+\\.no\$",
+        message = "clientName must match '*@client.*.no'"
+    )
     val clientName: String
 )
