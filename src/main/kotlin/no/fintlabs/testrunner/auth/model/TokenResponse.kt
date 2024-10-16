2f6df29748bf0544a9d87ea1bbe14878928957b1
Hknots <hknots99@gmail.com>
2024-10-16 11:00:02 +0200

    add JsonProperties

--- src/main/kotlin/no/fintlabs/testrunner/auth/model/TokenResponse.kt
+++ src/main/kotlin/no/fintlabs/testrunner/auth/model/TokenResponse.kt
@@ -1,9 +1,11 @@
 package no.fintlabs.testrunner.auth.model
 
+import com.fasterxml.jackson.annotation.JsonProperty
+
 data class TokenResponse(
-    val accessToken: String,
-    val tokenType: String,
-    val expiresIn: Int,
-    val acr: String,
-    val scope: String
+    @JsonProperty("access_token") val accessToken: String,
+    @JsonProperty("token_type") val tokenType: String,
+    @JsonProperty("expires_in") val expiresIn: Int,
+    @JsonProperty("acr") val acr: String,
+    @JsonProperty("scope") val scope: String
 )
