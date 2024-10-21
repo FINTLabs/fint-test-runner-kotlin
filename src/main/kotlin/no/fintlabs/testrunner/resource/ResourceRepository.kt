81acc0e93e2722339687ec28d93facb208ed5322
Hknots <hknots99@gmail.com>
2024-10-21 12:37:08 +0200

    remove reduntant logging

--- src/main/kotlin/no/fintlabs/testrunner/resource/ResourceRepository.kt
+++ src/main/kotlin/no/fintlabs/testrunner/resource/ResourceRepository.kt
@@ -7,11 +7,7 @@ class ResourceRepository {
 
     val resources: MutableMap<String, MutableList<String>> = mutableMapOf()
 
-    fun getResources(endpoint: String): MutableList<String>? {
-        var createComponentKey = createComponentKey(endpoint)
-        println("componentKey: $createComponentKey resources: $resources")
-        return resources[createComponentKey(endpoint)]
-    }
+    fun getResources(endpoint: String): MutableList<String>? = resources[createComponentKey(endpoint)]
 
     fun createComponentKey(endpoint: String) = endpoint.split("/").let { "${it[1]}.${it[2]}" }
 
