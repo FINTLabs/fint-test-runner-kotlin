626f5f658214b475347236d4fd9318bcd199f7fc
Hknots <hknots99@gmail.com>
2024-11-11 12:27:48 +0100

    add createdAt field

--- src/main/kotlin/no/fintlabs/testrunner/auth/model/TokenResponse.kt
+++ src/main/kotlin/no/fintlabs/testrunner/auth/model/TokenResponse.kt
@@ -1,11 +1,13 @@
 package no.fintlabs.testrunner.auth.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import java.util.Date
 
 data class TokenResponse(
     @JsonProperty("access_token") val accessToken: String,
     @JsonProperty("token_type") val tokenType: String,
     @JsonProperty("expires_in") val expiresIn: Int,
     @JsonProperty("acr") val acr: String,
-    @JsonProperty("scope") val scope: String
+    @JsonProperty("scope") val scope: String,
+    var createdAt: Long
 )
